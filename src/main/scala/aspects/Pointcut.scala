76dccbe66998170f5f15c6f27eeac005515eff1a
Justin Deters <justindeters@gmail.com>
2022-03-16 12:10:38 -0500

    Added regex pointcut

--- src/main/scala/aspects/Pointcut.scala
+++ src/main/scala/aspects/Pointcut.scala
@@ -11,6 +11,10 @@ object Pointcut {
     for(t <- fsm.alphabet) yield ((source, t))
   }
 
+  def byState(source: Regex)(implicit fsm: FeatureOrientedFSM) = {
+    for(t <- fsm.alphabet; s <- fsm.nameMap.filter(source matches _._1)) yield ((s._2,t))
+  }
+
   def byToken(token: Token)(implicit fsm: FeatureOrientedFSM) = {
     for(s <- fsm.states) yield (s, token)
   }
@@ -18,6 +22,18 @@ object Pointcut {
   def byDestination(destination: State)(implicit fsm: FeatureOrientedFSM) = {
     fsm.transitions.foldLeft(Set[(State, Token)]())((keySet, m) => {
       if(m._2 contains destination) {
+        keySet + m._1
+      } else {
+        keySet
+      }
+    })
+  }
+
+  def byDestination(destination: Regex)(implicit fsm: FeatureOrientedFSM) = {
+    val stateSet = (for(s <- fsm.nameMap.filter(destination matches _._1)) yield (s._2)).toSet
+
+    fsm.transitions.foldLeft(Set[(State, Token)]())((keySet, m) => {
+      if(!(m._2 & stateSet).isEmpty) {
         keySet + m._1
       } else {
         keySet
