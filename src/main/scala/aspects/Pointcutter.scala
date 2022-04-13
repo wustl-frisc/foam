74572ad4011d881b5c0f3238b8e89cd960e090e4
Max Camp-Oberhauser <c.max@wustl.edu>
2022-04-13 12:17:54 -0500

    Set -> Pointcut

--- src/main/scala/aspects/Pointcutter.scala
+++ src/main/scala/aspects/Pointcutter.scala
@@ -3,6 +3,6 @@ package aspects
 
 object Pointcutter {
   def apply[A, B <: A](base: Iterable[A], pred: A => Boolean): Pointcut[B] = {
-    base.foldLeft(Pointcut[A]())((pointcut, joinpoint) => if (pred(joinpoint)) pointcut + joinpoint else pointcut).asInstanceOf[Set[B]]
+    base.foldLeft(Pointcut[A]())((pointcut, joinpoint) => if (pred(joinpoint)) pointcut + joinpoint else pointcut).asInstanceOf[Pointcut[B]]
   }
 }
