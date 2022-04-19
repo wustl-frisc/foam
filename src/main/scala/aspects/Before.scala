1fef842177069c4b0a53f9c8204a956c10185aa2
Justin Deters <justindeters@gmail.com>
2022-04-18 22:52:18 -0500

    Fixed before advice

--- src/main/scala/aspects/Before.scala
+++ src/main/scala/aspects/Before.scala
@@ -6,7 +6,20 @@ import fsm._
 object Before {
   def apply[A <: State](pointcut: Pointcut[A], base: NFA)(body: A => (State, Token)) = {
     Advice[A, NFA](pointcut, base)((prevBase, jp) => {
-      prevBase.addTransition((body(jp)._1, body(jp)._2), jp)
+      val newKey = body(jp)
+
+      val transitionPointcut = Pointcutter[TransitionKey, TransitionKey](prevBase.transitions.keys,
+        key => key match {
+          case k: TransitionKey if((prevBase.getTransitions(k) contains jp) &&
+            k != newKey) => true
+          case _ => false
+      })
+
+      val step1 = Around[TransitionKey](transitionPointcut, prevBase)((thisJoinPoint: TransitionKey) => {
+        prevBase.getTransitions(thisJoinPoint) - base.error - jp + newKey._1
+      })
+
+      step1.clearTransitions(newKey).addTransitions(newKey, step1.getTransitions(newKey) - base.error + jp)
     })
   }
 }
