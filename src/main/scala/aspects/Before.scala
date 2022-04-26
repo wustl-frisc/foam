3ed7e8fe5a03b1daf80e92e641f4f5ddf3cdaaec
Justin Deters <justindeters@gmail.com>
2022-04-26 18:04:03 -0500

    Updating before advice for dynamic joinpoints and created print funds

--- src/main/scala/aspects/Before.scala
+++ src/main/scala/aspects/Before.scala
@@ -4,22 +4,27 @@ package aspects
 import fsm._
 
 object Before {
-  def apply[A <: State](pointcut: Pointcut[A], base: NFA)(body: A => (State, Token)) = {
-    Advice[A, NFA](pointcut, base)((prevBase, jp) => {
-      val newKey = body(jp)
+  def apply[A <: State](pointcut: Pointcut[A], base: NFA)(body: (Joinpoint[A], NFA) => (Option[(State, Token)], NFA)) = {
+    Advice[A, NFA](pointcut, base)((prevBase, point) => {
 
-      val transitionPointcut = Pointcutter[TransitionKey, TransitionKey](prevBase.transitions.keys,
-        key => key match {
-          case k: TransitionKey if((prevBase.getTransitions(k) contains jp) &&
-            k != newKey) => true
-          case _ => false
+      val ins = prevBase.getIns(point)
+
+      val joinPoints = for(in <- ins) yield (Joinpoint[A](point, Some(in), None))
+
+      joinPoints.foldLeft(prevBase)((newBase, jp) => {
+        val (advice, newNFA) = body(jp, newBase)
+
+        advice match {
+          case None => newNFA
+          case Some(path) => {
+            var (stateAdvice, tokenAdvice) = path
+
+            newNFA.removeTransition(jp.in.get, point)
+            .addTransition(jp.in.get, stateAdvice)
+            .addTransition((stateAdvice, tokenAdvice), point)
+          }
+        }
       })
-
-      val step1 = Around[TransitionKey](transitionPointcut, prevBase)((thisJoinPoint: TransitionKey) => {
-        prevBase.getTransitions(thisJoinPoint) - base.error - jp + newKey._1
-      })
-
-      step1.clearTransitions(newKey).addTransitions(newKey, step1.getTransitions(newKey) - base.error + jp)
     })
   }
 }
