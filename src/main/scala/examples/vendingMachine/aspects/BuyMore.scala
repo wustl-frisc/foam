7be94dd1612865186cebbb994fcbee9781b33172
Justin Deters <justindeters@gmail.com>
2022-04-27 12:04:50 -0500

    Added BuyMore feature

---
+++ src/main/scala/examples/vendingMachine/aspects/BuyMore.scala
@@ -0,0 +1,27 @@
+package edu.wustl.sbs
+package examples
+
+import fsm._
+import aspects._
+
+class BuyMore extends Aspect[NFA] {
+  def apply(nfa: NFA) = {
+    val dispensePointcut = Pointcutter[State, DispenseState](nfa.states, state => state match {
+      case s: DispenseState => true
+      case _ => false
+    })
+
+    Around[DispenseState](dispensePointcut, nfa)((thisJoinPoint: Joinpoint[DispenseState], thisNFA: NFA) => {
+      val source = thisJoinPoint.in.get._1 match {
+        case s: PrinterState => s.source.get
+        case s: State => s
+      }
+
+      val fundsLeft = source.asInstanceOf[ValueState].value - thisJoinPoint.point.product.value
+      val newDispense = DispenseState(thisJoinPoint.point.product, Some(source), thisJoinPoint.point.isAccept)
+      val newNFA = thisNFA.addTransition((newDispense, Lambda), ValueState(fundsLeft, source.isAccept))
+
+      (newDispense, newNFA)
+    })
+  }
+}
