3f4064e87d4dd12b6d47442480348786b955caf4
Justin Deters <justindeters@gmail.com>
2022-05-10 14:41:42 -0500

    eliminate unreachable states in DFA

--- src/main/scala/fsm/DFA.scala
+++ src/main/scala/fsm/DFA.scala
@@ -7,7 +7,10 @@ class DFA(input: FSM, error: State) extends FSM {
     override def start: State = input.start
     override def alphabet = input.alphabet
 
-    private var newStates: Set[State] = input.states
+    //get rid of unreachable states here
+    private var newStates: Set[State] = input.transitions.foldLeft(Set[State](start))((prevSet, transition) => {
+      prevSet ++ transition._2
+    })
     private var newTransitions: Map[TransitionKey,Set[State]] = input.transitions
     addTransitionsToError()
     newStates = newStates + error
@@ -42,7 +45,7 @@ class DFA(input: FSM, error: State) extends FSM {
     }
 
     private def addTransitionsToError() = {
-        for (token <- alphabet) {
+        for (token <- alphabet - Lambda) {
             for (state <- newStates) {
                 val destOption = newTransitions(state, token)
                 if (destOption.isEmpty) {
--- src/main/scala/fsm/Emitter.scala
+++ src/main/scala/fsm/Emitter.scala
@@ -13,26 +13,16 @@ object Emitter {
     dot.node(" ", attrs=Map("shape" -> "plain"))
     dot.edge(" ", namer(fsm.start))
 
-    var reachableStates = Set[State](fsm.start)
-
-    //find reachableStates
     for (((source, token) -> destinationSet) <- fsm.transitions) {
-      for (destination <- destinationSet) {
-        reachableStates += destination
-      }
-    }
-
-    for (((source, token) -> destinationSet) <- fsm.transitions) {
-      if(reachableStates contains source) {
+      if(fsm.states contains source) {
         for (destination <- destinationSet) {
           dot.edge(namer(source), namer(destination), label = namer(token))
         }
       }
     }
 
-    val states = reachableStates
-    for (state <- states.filter(_.isAccept)) dot.node(namer(state), attrs = Map("shape" -> "doublecircle"))
-    for (state <- states.filter(_.isAccept == false)) dot.node(namer(state), attrs = Map("shape" -> "circle"))
+    for (state <- fsm.states.filter(_.isAccept)) dot.node(namer(state), attrs = Map("shape" -> "doublecircle"))
+    for (state <- fsm.states.filter(_.isAccept == false)) dot.node(namer(state), attrs = Map("shape" -> "circle"))
 
     dot.view(fileName = "fsm.gv", directory = ".")
   }
