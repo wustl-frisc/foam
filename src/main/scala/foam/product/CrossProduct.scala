ffd101f484353fe52d9ec5c8e69721783a0e3638
Peyton Gozon <peyton.gozon@wustl.edu>
2022-07-11 16:18:23 -0500

    Added the Product Machine -- allows for unions and conjunctions

---
+++ src/main/scala/foam/product/CrossProduct.scala
@@ -0,0 +1,5 @@
+package foam.product
+
+object CrossProduct {
+
+}
---
+++ src/main/scala/foam/product/Product.scala
@@ -0,0 +1,34 @@
+package foam.product
+
+import foam.{NFA, State}
+
+object Product {
+  def apply(m1: NFA, m2: NFA, isIntersectionMachine: Boolean): NFA = {
+
+    assert(m1.alphabet == m2.alphabet, "The alphabet of both machines must be the same for a Product Machine")
+
+    val combinedStartState = CombinedState(m1.start, m2.start, isIntersectionMachine)
+    var oldMachine = new NFA(combinedStartState)
+    var newMachine = oldMachine
+
+    do {
+      oldMachine = newMachine
+
+      for(((m1SrcState, m1Transition), m1DstStates) <- m1.transitions;
+          ((m2SrcState, m2Transition), m2DstStates) <- m2.transitions) {
+
+        if (m1Transition == m2Transition) {
+          val combinedSrcState = CombinedState(m1SrcState, m2SrcState, isIntersectionMachine)
+          val transitionToken = m1Transition
+
+          for (m1DstState <- m1DstStates; m2DstState <- m2DstStates) {
+            val combinedDstState = CombinedState(m1DstState, m2DstState, isIntersectionMachine)
+            newMachine = newMachine.addTransition((combinedSrcState, transitionToken), combinedDstState)
+          }
+        }
+      }
+    } while (!oldMachine.isEqual(newMachine))
+
+    newMachine
+  }
+}
