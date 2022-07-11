315309083c2af48f58e5d01102079953eb863d16
Peyton Gozon <peyton.gozon@wustl.edu>
2022-07-11 16:48:04 -0500

    Added an UnApply method to CombinedState

--- src/main/scala/foam/product/CombinedState.scala
+++ src/main/scala/foam/product/CombinedState.scala
@@ -5,4 +5,5 @@ import foam.State
 case class CombinedState(s1: State, s2: State, conjunction: Boolean) extends State {
   override def isAccept: Boolean = if (conjunction) s1.isAccept && s2.isAccept else s1.isAccept || s2.isAccept
   override def toString: String = s1.toString + "," + s2.toString
+  def unApply: (State, State) = (s1, s2)
 }
