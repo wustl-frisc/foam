66e4cd5eda3fbc1808e6e749b4aaa458c5261c33
Peyton Gozon <peyton.gozon@wustl.edu>
2022-07-11 16:52:31 -0500

    Naming consistency

--- src/main/scala/foam/product/Product.scala
+++ src/main/scala/foam/product/Product.scala
@@ -1,13 +1,13 @@
 package foam.product
 
-import foam.{NFA, State}
+import foam.NFA
 
 object Product {
-  def apply(m1: NFA, m2: NFA, isIntersectionMachine: Boolean): NFA = {
+  def apply(m1: NFA, m2: NFA, isConjunctionMachine: Boolean): NFA = {
 
     assert(m1.alphabet == m2.alphabet, "The alphabet of both machines must be the same for a Product Machine")
 
-    val combinedStartState = CombinedState(m1.start, m2.start, isIntersectionMachine)
+    val combinedStartState = CombinedState(m1.start, m2.start, isConjunctionMachine)
     var oldMachine = new NFA(combinedStartState)
     var newMachine = oldMachine
 
@@ -18,11 +18,11 @@ object Product {
           ((m2SrcState, m2Transition), m2DstStates) <- m2.transitions) {
 
         if (m1Transition == m2Transition) {
-          val combinedSrcState = CombinedState(m1SrcState, m2SrcState, isIntersectionMachine)
+          val combinedSrcState = CombinedState(m1SrcState, m2SrcState, isConjunctionMachine)
           val transitionToken = m1Transition
 
           for (m1DstState <- m1DstStates; m2DstState <- m2DstStates) {
-            val combinedDstState = CombinedState(m1DstState, m2DstState, isIntersectionMachine)
+            val combinedDstState = CombinedState(m1DstState, m2DstState, isConjunctionMachine)
             newMachine = newMachine.addTransition((combinedSrcState, transitionToken), combinedDstState)
           }
         }
